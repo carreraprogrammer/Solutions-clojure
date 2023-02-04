c89b70fbf4c431c9e51b5be6c724b70f36ae3f57
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-04 14:00:55 -0500

    Add test for negative price

--- src/invoice_item_test.clj
+++ src/invoice_item_test.clj
@@ -6,3 +6,8 @@
   (let [item {:invoice-item/precise-quantity 5
               :invoice-item/precise-price 15}]
     (is (= 75.0 (subtotal item)))))
+
+(deftest test-with-negative-price
+  (let [item {:invoice-item/precise-quantity 20
+              :invoice-item/precise-price -20}]
+    (is (= -400.0 (subtotal item)))))
