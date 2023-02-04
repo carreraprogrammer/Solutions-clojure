fa4e77cd3311a77d6e5feac0f2065f939f07b3aa
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-04 08:18:19 -0500

    Add function with the conditions to be satisfied

--- src/problem-1.clj
+++ src/problem-1.clj
@@ -1,17 +1,18 @@
 
-; Find the items that satisfy the next conditions
-
-;; 1. At least have one item that has :iva 19%
-;; 2. At least one item has retention :ret_fuente 1%
-;; 3. Every item must satisfy EXACTLY one of the above two conditions. This means that an item cannot have BOTH :iva 19% and retention :ret_fuente 1%.
+(defn conditions
+  [item]
+  "Generate the conditions that satisfy the requirements of the problem"
+  (let [taxes (:taxable/taxes item)
+        retentions (:retentionable/retentions item)]
+    (or (and (some (fn [tax] (= 19 (:tax/rate tax))) taxes) (not (some (fn [ret] (= 1 (:retention/rate ret))) retentions)))
+        (and (not (some (fn [tax] (= 19 (:tax/rate tax))) taxes)) (some (fn [ret] (= 1 (:retention/rate ret))) retentions)))))
 
 (defn filter-invoice
   "Filter the invoice items that satisfy the conditions provided"
   [invoice]
   (filter conditions (:invoice/items invoice)))             ;; (filter CONDITIONS COLLECTION)
 
-;; 2. The COLECTION argument is going to be the Invoice, so the filter function should be inside another function that takes as argument a COLLECTION in this case Invoice
-;;; - (defn filter-invoice [Invoice] (filter CONDITIONS [Invoice]))
+
 ;; 3. What are the conditions?
 ;;; - The first condition is that the item should have at least one iva = 19 % OR :ret-fuente 1%
 ;;; - The second condition is that the item should have EXACTLY one of the two conditions
