97dd362b9e4a65991ca95225d2d08f92160c2a75
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-04 10:31:58 -0500

    Acces to the items vector

--- src/problem_2.clj
+++ src/problem_2.clj
@@ -5,36 +5,24 @@
 (def invoice-str (slurp "invoice.json"))
 (def data (json/read-str invoice-str :key-fn keyword))
 
+;; I need a function that lend me extract data from a high level key, to iterate and put it in my new invoice
+
+(defn extract-values [items keys]
+  (map #(select-keys % keys) items))
+
 (defn validate-invoice [data]
   (let [invoice (:invoice data)
         customer (:customer invoice)
         name (str (:company_name customer))
         email (str (:email customer))
-        issue-date (:issue_date invoice)]
+        issue-date (:issue_date invoice)
+        items (:items invoice)]
     {:invoice
      {
       :issue_date issue-date
       :invoice/customer {:customer/name name
                          :customer/email email}
-      :invoice/items [{
-                       :invoice-item/price ""
-                       :invoice-item/quantity ""
-                       :invoice-item/sku ""
-                       :invoice-item/taxes []
-                       }
-                      {
-                       :invoice-item/price ""
-                       :invoice-item/quantity ""
-                       :invoice-item/sku ""
-                       :invoice-item/taxes []
-                       }
-                      {
-                       :invoice-item/price ""
-                       :invoice-item/quantity ""
-                       :invoice-item/sku ""
-                       :invoice-item/taxes []
-                       }
-                      ]
+      :invoice/items items
       }}
     ))
 
