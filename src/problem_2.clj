75be1d992c99bd9330dc409fd90957f0ef2861bb
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-04 12:19:46 -0500

    Create functions to transform a string into a instant

--- src/problem_2.clj
+++ src/problem_2.clj
@@ -1,9 +1,25 @@
 (ns problem-2
   (:require [clojure.spec.alpha :as s]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [invoice_spec :as test]
+            ))
+(use 'clojure.instant)
+
 
 (def invoice-str (slurp "invoice.json"))
 (def data (json/read-str invoice-str :key-fn keyword))
+
+(read-instant-date "2017-08-23")
+
+;; The first function transform a time with this format "08/03/2017" into a vector [08 03 2017]
+
+(defn format-date-v2 [date-vec]
+  (str (nth date-vec 2) "-" (nth date-vec 1) "-" (nth date-vec 0)))
+
+;; The second function uses the vector and transform the date in "2017-08-03"
+(defn transform-date [date-str]
+  (let [date-vec (mapv #(Integer/parseInt %) (clojure.string/split date-str #"/"))]
+    (format-date-v2 date-vec)))
 
 (defn validate-invoice [data]
   (let [invoice (:invoice data)
@@ -21,13 +37,13 @@
                       :invoice-item/quantity (double quantity)
                       :invoice-item/:sku (str sku)
                       :invoice-item/:taxes [
-                                            :tax/category :iva
-                                            :tax/rate (double rate)
+                                            {:tax/category :iva
+                                             :tax/rate (double rate)}
                                             ]
                           }])]
-    {:invoice
+     {:invoice
      {
-      :issue_date issue-date
+      :invoice/issue-date (read-instant-date (transform-date issue-date))
       :invoice/customer {:customer/name name
                          :customer/email email}
       :invoice/items items
@@ -35,3 +51,5 @@
     ))
 
 (validate-invoice data)
+
+(s/valid? ::test/invoice (validate-invoice data))
